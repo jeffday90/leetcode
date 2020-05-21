fbd8deeefb393ccd95eb700bd8a3920a1654669a
Jeff Day <jeffrey.day00@gmail.com>
2020-05-21 15:36:02 -0700

    Simple multiplication without *

---
+++ multiplcationWithout.js
@@ -0,0 +1,11 @@
+// create a function that does multiplation without using 
+
+function multiplation(n, m) {
+    let result = 0;
+
+    for (let i = 0; i <= m; i++) {
+        result += n;
+    }
+
+    return result;
+}
